f0761acf3acf7f7ae40a7728e1d5ce5aaa55f0f0
Sam <sam210723@hotmail.com>
2018-12-14 16:52:44 +1000

    UDP socket

--- decoder/orbcomm_decoder.py
+++ decoder/orbcomm_decoder.py
@@ -7,9 +7,14 @@ https://github.com/sam210723/orbcomm-rx
 import argparse
 from colorama import init as init_color
 from colorama import Fore, Back, Style
+from socket import *
+import stx
 import tools
 
 ver = 1.0
+udpIP = "0.0.0.0"
+udpTimeout = 10
+udpSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Parse arguments
 argparser = argparse.ArgumentParser(description="Orbcomm STX Decoder v{0}".format(ver))
@@ -23,7 +28,22 @@ def init():
     print("┃" + Style.BRIGHT + " http://github.com/sam210723/orbcomm-rx " + Style.RESET_ALL + "┃")
     print("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛\n")
 
-    print("Listening on port {0}".format(args.p))
+    start_socket()
+
+
+def start_socket():
+    # Bind socket to port on all interfaces
+    udpSocket.settimeout(udpTimeout)
+    udpSocket.bind((udpIP, int(args.p)))
+    print("UDP listening on port {0}\n".format(args.p))
+
+    while True:
+        try:
+            data, addr = udpSocket.recvfrom(1024)
+            stx.decode(data)
+        except timeout:
+            print("No data received for {0} seconds".format(udpTimeout))
+            break
 
 
 init_color()
---
+++ decoder/stx.py
@@ -0,0 +1,9 @@
+"""
+stx.py
+Orbcomm STX Decoder v1.0
+https://github.com/sam210723/orbcomm-rx
+"""
+
+
+def decode(data):
+    print(data)
